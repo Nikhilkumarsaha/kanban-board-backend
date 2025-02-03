8501319b06fbb5a293d265380d865e1914001bf0
Nikhil kumar saha <nikhilsaha92236@gmail.com>
2025-02-03 13:24:08 +0530

    .env configuration

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const Board = require('./models/Board');
+require('dotenv').config()
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://nikhilsaha:KanbanBoard@cluster0.7dpfl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
